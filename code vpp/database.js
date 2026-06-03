23e7022fd9222ab7d452ecd603e6fa972d6ce1eb
Tran-Quoc-Anh-Dung <anhdung200090@gmail.com>
2026-06-03 12:11:23 +0700

    up

--- code vpp/database.js
+++ code vpp/database.js
@@ -1,17 +1,19 @@
+require("dotenv").config();
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "VPP",
-  user: "ANHDUNG",
-  password: "123",
-  database: "pos_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect(err => {
   if (err) {
-    console.log("Lỗi kết nối:", err);
+    console.log("❌ Lỗi kết nối:", err);
   } else {
-    console.log("Kết nối MySQL thành công");
+    console.log("✅ Kết nối MySQL online thành công");
   }
 });
 

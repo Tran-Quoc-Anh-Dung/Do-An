3f18f4b34d9b8e74d4087f67b57f1e929a0aec5b
Tran-Quoc-Anh-Dung <anhdung200090@gmail.com>
2026-06-03 11:45:19 +0700

    UP

--- code vpp/database.js
+++ code vpp/database.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: "VPP",
+  user: "ANHDUNG",
+  password: "123",
   database: "pos_db",
 });
 

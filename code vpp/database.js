dd093e0735684993b3fd463f5227269941abe09d
Tran-Quoc-Anh-Dung <anhdung200090@gmail.com>
2026-06-03 13:05:29 +0700

    .

--- code vpp/database.js
+++ code vpp/database.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 const mysql = require("mysql2");
 console.log("ENV CHECK:", process.env.DB_USER, process.env.DB_PASSWORD);
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT
 });
 
 db.connect(err => {
